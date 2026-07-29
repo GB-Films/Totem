5dfff1ea094bb3891fa7b5372712a21ad88f9d90
Guido Boetsch <gboetsch93@gmail.com>
2026-07-29 20:15:05 -0300

    Updates

--- assets/save-CIVZVLsj.js
+++ assets/save-CIVZVLsj.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CwoAoOaQ.js";/**
+import{c as a}from"./index-BEz6TuUu.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
