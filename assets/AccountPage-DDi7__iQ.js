29934b8ab70348b738a99ba1c3ce7779f5612588
Guido Boetsch <gboetsch93@gmail.com>
2026-07-29 15:25:00 -0300

    Updates

--- assets/AccountPage-DDi7__iQ.js
+++ assets/AccountPage-DDi7__iQ.js
@@ -1,4 +1,4 @@
-import{c as F,e as E,b as H,a as O,g as L,j as e,H as d,O as D,q as T,U as w}from"./index-BR0ySVd3.js";import{b as l,L as o}from"./react-BdHa0J-c.js";import{d as G,e as P,l as Q,k as U}from"./firebase-9RZYwvJ5.js";import{R as B}from"./ReservationHistory-DR6CxwfL.js";import{L as V}from"./lock-C52oG8ZB.js";import{L as W,S as J}from"./save-BF45-W0i.js";import"./format-x7npCmSH.js";import"./messages-CXmJcgZr.js";import"./message-circle-DgFLP46Q.js";/**
+import{c as F,e as E,b as H,a as O,g as L,j as e,H as d,O as D,q as T,U as w}from"./index-w00OXKOw.js";import{b as l,L as o}from"./react-BdHa0J-c.js";import{d as G,e as P,l as Q,k as U}from"./firebase-9RZYwvJ5.js";import{R as B}from"./ReservationHistory-DMam0ZNN.js";import{L as V}from"./lock-CPIbBT_q.js";import{L as W,S as J}from"./save-CAmiAbFS.js";import"./format-x7npCmSH.js";import"./messages-C6Ek_mnp.js";import"./message-circle-Bb449f66.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
