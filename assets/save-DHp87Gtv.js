f6a620f6bac2f5843d19e9c6c4de9e980537a35c
Guido Boetsch <gboetsch93@gmail.com>
2026-07-30 10:49:28 -0300

    Updates

--- assets/save-DHp87Gtv.js
+++ assets/save-DHp87Gtv.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BQZQ6Xoa.js";/**
+import{c as a}from"./index-CP3SUaLq.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-CV_m2s57.js
+++ assets/trash-2-CV_m2s57.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-BQZQ6Xoa.js";/**
+import{c as e}from"./index-CP3SUaLq.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
